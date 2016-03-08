e663a67cb5f98811b08336f201cb4ca5aa9df5b3
Henry Johnson <nzhenry@gmail.com>
2016-03-08 18:45:36 -0500

    try again

--- create.sh
+++ create.sh
@@ -16,17 +16,8 @@ fi
 docker-machine create --driver digitalocean --digitalocean-access-token ${API_KEY} --digitalocean-region tor1 --digitalocean-size 512mb jenkins-host
 
 # create jenkins app
-docker-machine ssh jenkins-host "apt-get update && \
-	apt-get install -y haveged && \
-	mkdir /root/certs && \
-	openssl req -subj '/CN=hjohnsondev.com/O=Henry Johnson/C=CA' \
-		-new -newkey rsa:2048 -days 365 -nodes -x509 \
-		-keyout /root/certs/hjohnsondev.com.key \
-		-out /root/certs/hjohnsondev.com.crt && \
-	curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` \
-		> /usr/local/bin/docker-compose && \
-	chmod +x /usr/local/bin/docker-compose && \
-	git clone https://github.com/nzhenry/docker-jenkins.git && \
+docker-machine ssh jenkins-host \
+	"git clone https://github.com/nzhenry/docker-jenkins.git && \
 	cd /root/docker-jenkins && \
+	sh setup.sh && \
 	docker-compose up"
-	
---
+++ setup.sh
@@ -0,0 +1,22 @@
+
+# make sure the package manager is up to date
+apt-get update &&
+
+# add entropy. this is needed for cloud instances
+apt-get install -y haveged &&
+
+# create directory for ssl certs
+mkdir /root/certs &&
+
+# create ssl certs
+openssl req -subj '/CN=hjohnsondev.com/O=Henry Johnson/C=CA' \
+	-new -newkey rsa:2048 -days 365 -nodes -x509 \
+	-keyout /root/certs/hjohnsondev.com.key \
+	-out /root/certs/hjohnsondev.com.crt &&
+
+# download docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.6.2/docker-compose-`uname -s`-`uname -m` \
+	> /usr/local/bin/docker-compose &&
+
+# make it executable
+chmod +x /usr/local/bin/docker-compose
