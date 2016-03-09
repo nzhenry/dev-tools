43a2ea797bfafdf0ea7ff280ada4d3f0ca1ea7ac
Henry Johnson <nzhenry@gmail.com>
2016-03-09 14:47:25 -0500

    test commit

--- setup.sh
+++ setup.sh
@@ -21,5 +21,5 @@ openssl req -subj '/CN=jenkins.livehen.com/O=Henry Johnson/C=CA' \
 # install ssl proxy
 docker run -d -p 80:80 -p 443:443 -v /root/certs:/etc/nginx/certs -v /var/run/docker.sock:/tmp/docker.sock:ro jwilder/nginx-proxy &&
 
-# install jenkins
-docker run -d -e VIRTUAL_HOST=jenkins.livehen.com -e VIRTUAL_PORT=8080 -v jenkins_home:/var/jenkins_home jenkins
+# install jenkins with docker
+docker run --name jenkins -d -e VIRTUAL_HOST=jenkins.livehen.com -e VIRTUAL_PORT=8080 -v jenkins_home:/var/lib/jenkins -v /var/run/docker.sock:/var/run/docker.sock killercentury/jenkins-dind
